69c0fe2af6390a5795ea62208e27dab8e39e575b
Christian Engelhardt <cengelha.appdevl@outlook.com>
2022-03-03 16:27:15 +0100

    Update UP_MCGUN2

--- read_data.py
+++ read_data.py
@@ -1350,9 +1350,9 @@ DATA = {
     'UP_MCGUN': {
         '2': {
             'meta': [
-                ('Vehicle_GunDamage', 3, 7),
+                ('Vehicle_GunDamage', 3, 6),
                 ('Vehicle_GunHeatTime', 6, 10),
-                ('Vehicle_GunRate', 5, 10),
+                ('Vehicle_GunRate', 3, 5),
             ],
             'number': 3,  # 2
         },
